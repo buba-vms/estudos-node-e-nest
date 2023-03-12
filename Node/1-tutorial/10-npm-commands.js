d6a7a5a14b7611a8edeb539052ebf218afed7eca
buba-vms <victor.buba@live.com>
2023-03-11 21:48:12 -0300

    express tutorial

--- Node/1-tutorial/10-npm-commands.js
+++ Node/1-tutorial/10-npm-commands.js
@@ -4,6 +4,9 @@
 // local dependency - use it only for this project
 // npm i <packageName>
 
+// dev dependency - when you don't want to use it in production
+// npm i <packageName> -D
+
 // global dependcy - use it in any project
 // npm install -g <packageName>
 // sudo install -g <packageName> (mac)
@@ -19,3 +22,4 @@ const items = [1, [2, [3, [4]]]]
 
 const newItems = _.flattenDeep(items)
 console.log(newItems)
+console.log('hello world')
